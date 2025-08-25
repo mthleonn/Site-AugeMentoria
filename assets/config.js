68b948c4484ec12cd0802710f6c15a3a9a299345
mthleonn <mthcpx22@gmail.com>
2025-08-25 17:08:17 -0300

    Remover arquivos não utilizados e atualizar configurações

--- assets/config.js
+++ assets/config.js
@@ -6,26 +6,22 @@ const ASSETS_CONFIG = {
     // Logos
     LOGO_AUGE: './assets/images/Logo_augesemfundo.jpg', // Logo oficial AUGE
     
-    // Imagens dos mentores
-    MENTOR_RAFAEL: './assets/images/Rafael.jpg', // Manter na raiz por enquanto
-    MENTOR_DANIEL: './assets/images/Daniel.jpg', // Manter na raiz por enquanto
+    // Imagens dos mentores (agora usando SVGs na raiz)
+    MENTOR_RAFAEL: './rafael.svg',
+    MENTOR_DANIEL: './daniel.svg',
     
     // Imagens dos módulos
     MODULO_1: './assets/images/1.jpg', // Capa do Módulo 1
     MODULO_2: './assets/images/2.jpg', // Capa do Módulo 2
     MODULO_3: './assets/images/3.jpg', // Capa do Módulo 3
     MODULO_4: './assets/images/4.jpg', // Capa do Módulo 4
-    MODULO_5: './assets/images/modulo5.jpg', // Placeholder - você pode substituir
-    MODULO_6: './assets/images/modulo6.jpg', // Placeholder - você pode substituir
+
     
     // Imagens de fundo
     COMPUTERS_BG: './computers-bg.svg', // Manter na raiz por enquanto
     
-    // Outras imagens que você pode adicionar
+    // Outras imagens utilizadas
     HERO_MENTORS: './assets/images/fotojuntos.jpg', // Foto dos mentores juntos na seção hero
-    HERO_IMAGE: './assets/images/hero-image.jpg',
-    TESTIMONIAL_1: './assets/images/testimonial1.jpg',
-    TESTIMONIAL_2: './assets/images/testimonial2.jpg',
     
     // Função helper para obter caminho completo
     getImagePath: function(imageName) {
